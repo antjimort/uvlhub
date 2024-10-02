e24502970988c06e9d95cf3c2ddd09613461d55b
Antonio Jiménez Ortega <antjimort@alum.us.es>
2024-10-02 18:51:42 +0200

    deleted .yml's

---
+++ .history/app/modules/auth/forms_20241002185046.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class SignupForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=100)])
+    surname = StringField('Surname', validators=[DataRequired(), Length(max=100)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Login')
+1
+        MARIADB_PORT: 3306
+        MARIADB_TEST_DATABASE: uvlhubdb_test
+        MARIADB_USER: uvlhub_user
+        MARIADB_PASSWORD: uvlhub_password
+        CODACY_PROJECT_TOKEN: ${{ secrets.CODACY_PROJECT_TOKEN }}
+
